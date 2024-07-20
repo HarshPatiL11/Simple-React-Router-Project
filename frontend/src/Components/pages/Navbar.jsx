77fd2bd0977faed5103d021ba22263f9c6ca19b7
HarshPatiL11 <patilharsh497@gmail.com>
2024-07-20 13:16:23 +0530

    dad

---
+++ frontend/src/Components/pages/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Css/Navbar.Css'
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+        <section>
+            <div>
+                <h1>Harsh</h1>
+            </div>
+            <div></div>
+        </section>
+    </nav>
+    </>
+)
+}
+
+export default Navbar
